493adf2f50546af7db8a9934ecc8ec71ed2693c0
jvdspeare <jvdspeare@gmail.com>
2019-01-18 23:53:25 +0000

    Megaport API Python Library

--- megalib.py
+++ megalib.py
@@ -15,21 +15,13 @@ def env(prod):
 # api get method template
 def get(url, header):
     response = requests.get(url, headers=header)
-    if response.status_code == 200:
-        json = response.json()
-    else:
-        json = None
-    return json
+    return response
 
 
 # api post method template
 def post(url, header, body):
     response = requests.post(url, headers=header, json=body)
-    if response.status_code == 200 or 400:
-        json = response.json()
-    else:
-        json = None
-    return json
+    return response
 
 
 # login and if successful return header to be used for subsequent api calls
@@ -45,9 +37,9 @@ def login(user, pasw, tfa=None, prod=True):
         json = response.json()
         token = json["data"]["token"]
         header = {'X-Auth-Token': token, 'Content-Type': 'application/json'}
+        return header
     else:
-        header = None
-    return header
+        return response
 
 
 # locations end point will return a list of data centre locations and information pertaining to each
@@ -68,7 +60,7 @@ def ix(header, loc_id, prod=True):
     return get(url, header)
 
 
-# port validate
+# validate an order for a megaport
 def port_validate(header, loc_id, name, speed, market, term=1, prod=True):
     url = env(prod) + '/v2/networkdesign/validate'
     body = [{'locationId': loc_id,
@@ -84,8 +76,44 @@ def port_validate(header, loc_id, name, speed, market, term=1, prod=True):
     return post(url, header, body)
 
 
+# place an order for a megaport
+def port_buy(header, loc_id, name, speed, market, term=1, prod=True):
+    url = env(prod) + '/v2/networkdesign/buy'
+    body = [{'locationId': loc_id,
+             'term': term,
+             'locationUid': 'null',
+             'productName': name,
+             'productType': 'MEGAPORT',
+             'createDate': int(time.time()),
+             'portSpeed': speed,
+             'virtual': 'false',
+             'market': market
+             }]
+    return post(url, header, body)
+
+
+# validate an order for a mcr
 def mcr_validate(header, loc_id, name, speed, market, asn=133937, term=1, prod=True):
     url = env(prod) + '/v2/networkdesign/validate'
+    body = [{'locationId': loc_id,
+             'term': term,
+             'locationUid': 'null',
+             'productName': name,
+             'productType': 'MEGAPORT',
+             'createDate': int(time.time()),
+             'portSpeed': speed,
+             'virtual': 'true',
+             'market': market,
+             'config': {
+                 'mcrAsn': asn
+             }
+             }]
+    return post(url, header, body)
+
+
+# place an order for a mcr
+def mcr_buy(header, loc_id, name, speed, market, asn=133937, term=1, prod=True):
+    url = env(prod) + '/v2/networkdesign/buy'
     body = [{'locationId': loc_id,
              'term': term,
              'locationUid': 'null',
