c1b53360eee8a7f44a6c07b40bcba6c20733eb3a
jvdspeare <jvdspeare@gmail.com>
2019-02-07 14:51:58 +0000

    Megaport API Python Library

---
+++ examples/mcr.py
@@ -0,0 +1,12 @@
+import megalib
+y = megalib.login(input('username'), input('password'))
+if y[0] == 200:
+    x = megalib.mcr(y[2], 68, 'mcr name', 1000, 'AU', validate=True)
+    if x[0] == 200:
+        print('monthly cost ' + str(x[2]))
+        z = megalib.port(y[2], 68, 'mcr name', 1000, 'AU')
+        print('Uid' + z[2])
+    else:
+        print('failed validation')
+else:
+    print('failed login')
