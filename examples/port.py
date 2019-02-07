4426ae123be0e138b87d3822140008f793fda875
jvdspeare <jvdspeare@gmail.com>
2019-02-07 14:36:17 +0000

    Megaport API Python Library

--- examples/port.py
+++ examples/port.py
@@ -1,10 +1,10 @@
 import megalib
 y = megalib.login(input('username'), input('password'))
 if y[0] == 200:
-    x = megalib.port(y[2], 29, 'megaport name', 1000, 'au', validate=True)
+    x = megalib.port(y[2], 32, 'megaport name', 1000, 'AU', validate=True)
     if x[0] == 200:
-        print('monthly cost ' + x[2])
-        z = megalib.port(y[2], 29, 'megaport name', 1000, 'au')
+        print('monthly cost ' + str(x[2]))
+        z = megalib.port(y[2], 32, 'megaport name', 1000, 'AU')
         print('Uid' + z[2])
     else:
         print('failed validation')
