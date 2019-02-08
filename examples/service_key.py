061d7384bc5a3755ab6402760ecc4fca49ed256e
jvdspeare <jvdspeare@gmail.com>
2019-02-08 11:49:36 +0000

    Megaport API Python Library

--- examples/service_key.py
+++ examples/service_key.py
@@ -2,7 +2,7 @@ import megalib
 x = megalib.login(input('username'), input('password'))
 
 if x[0] == 200:
-    y = megalib.service_key_lookup(x[2], 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx')
+    y = megalib.service_key(x[2], 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 'test service key')
     print(y[1].json())
 else:
     print('failed login')
