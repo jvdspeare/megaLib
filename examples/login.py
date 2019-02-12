f05580c162be5370e2a241732c4e33ddd817816d
jvdspeare <jvdspeare@gmail.com>
2019-02-12 15:49:20 +0000

    Megaport API Python Library

--- examples/login.py
+++ examples/login.py
@@ -1,9 +1,20 @@
+# Import megalib
 import megalib
 
-l = megalib.login(input('username'), input('password'), input('tfa'))
+# Authenticate user credentials using the megalib.login function
+auth = megalib.login(input('username: '), input('password: '), input('tfa (leave black if not enabled): '), prod=False)
 
-if l.status_code == 200:
-    print(l.header)
-
+# Check if logging was successful by observing the HTTP Status Code
+if auth.status_code == 200:
+    print('login successful')
 else:
     print('login failed')
+
+# HTTP Status Code
+print(l.status_code)
+
+# Response Body
+print(l.json)
+
+# Header
+print(l.header)
