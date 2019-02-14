89867bbe3bd72a6268360e4e928c3c361f0c5271
jvdspeare <jvdspeare@gmail.com>
2019-02-14 12:07:41 +0000

    Megaport API Python Library

--- examples/locations.py
+++ examples/locations.py
@@ -1,7 +1,13 @@
+# Import megalib
 import megalib
-y = megalib.login(input('username'), input('password'))
-if y[0] == 200:
-    x = megalib.locations(y[2])
-    print(x.json)
+
+# Authenticate user credentials using the megalib.login function
+auth = megalib.login(input('username: '), input('password: '), input('tfa (leave black if not enabled): '))
+
+# Verify that authentication was successful before attempting to retrieve locations
+if auth.status_code == 200:
+    print(megalib.locations(auth.header).json)
+
+# Advise user if the user authentication was unsuccessful
 else:
-    print('failed login')
+    print('login failed')
