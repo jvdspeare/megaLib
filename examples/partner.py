6a942f478e1bd183c82d8e480e3e75c07b0706fc
jvdspeare <jvdspeare@gmail.com>
2019-02-18 15:25:27 +0000

    Megaport API Python Library

--- examples/partner.py
+++ examples/partner.py
@@ -1,7 +1,25 @@
+# Import megalib
 import megalib
-y = megalib.login(input('username'), input('password'))
-if y[0] == 200:
-    x = megalib.partner(y[2])
-    print(x[1].json())
+
+# Authenticate user credentials using the megalib.login function
+auth = megalib.login(input('username: '), input('password: '), input('tfa (leave black if not enabled): '), prod=True)
+
+# Check if logging was successful by observing the HTTP Status Code
+if auth.status_code == 200:
+    print('login successful')
+
+    # Retrieve partner ports using the megalib.partner function
+    pports = megalib.partner(auth.header, prod=True)
+
+    # Check if the partner ports call was successful by observing the HTTP Status Code
+    if pports.status_code == 200:
+        # Print the body of the response
+        print(pports.json)
+
+    # Advise user if the partner ports call failed
+    else:
+        print('failed')
+
+# Advise user if login failed
 else:
-    print('failed login')
+    print('login failed')
