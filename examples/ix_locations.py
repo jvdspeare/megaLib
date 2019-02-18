d272fed85553bcefb1998d0dc550a53bc474c3e8
jvdspeare <jvdspeare@gmail.com>
2019-02-18 15:28:49 +0000

    Megaport API Python Library

--- examples/ix_locations.py
+++ examples/ix_locations.py
@@ -1,7 +1,25 @@
+# Import megalib
 import megalib
-y = megalib.login(input('username'), input('password'))
-if y[0] == 200:
-    x = megalib.ix_locations(y[2], 32)
-    print(x[1].json())
+
+# Authenticate user credentials using the megalib.login function
+auth = megalib.login(input('username: '), input('password: '), input('tfa (leave black if not enabled): '), prod=True)
+
+# Check if logging was successful by observing the HTTP Status Code
+if auth.status_code == 200:
+    print('login successful')
+
+    # Retrieve ix locations using the megalib.ix_locations function
+    loc = megalib.ix_locations(auth.header, input('location id: '), prod=True)
+
+    # Check if the ix locations call was successful by observing the HTTP Status Code
+    if loc.status_code == 200:
+        # Print the body of the response
+        print(loc.json)
+
+    # Advise user if the ix locations call failed
+    else:
+        print('failed')
+
+# Advise user if login failed
 else:
-    print('failed login')
+    print('login failed')
