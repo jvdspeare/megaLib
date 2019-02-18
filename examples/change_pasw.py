56145cf76caf0ea8a6c695342be3c03a2809aab8
jvdspeare <jvdspeare@gmail.com>
2019-02-18 15:14:35 +0000

    Megaport API Python Library

--- examples/change_pasw.py
+++ examples/change_pasw.py
@@ -1,6 +1,24 @@
+# Import megalib
 import megalib
-y = megalib.login(input('username'), input('password'))
-if y[0] == 200:
-    x = megalib.change_pasw(y[2], input('old password'), input('new password'))
+
+# Authenticate user credentials using the megalib.login function
+auth = megalib.login(input('username: '), input('password: '), input('tfa (leave black if not enabled): '), prod=True)
+
+# Check if logging was successful by observing the HTTP Status Code
+if auth.status_code == 200:
+    print('login successful')
+
+    # Change user password using the megalib.change_pasw function
+    passw = megalib.change_pasw(auth.header, 'old password: ', 'new password: ', prod=True)
+
+    # Check if password change was successful by observing the HTTP Status Code
+    if passw.status_code == 200:
+        print('password changed')
+
+    # Advise user if password change failed
+    else:
+        print('password change failed')
+
+# Advise user if login failed
 else:
-    print('failed login')
+    print('login failed')
