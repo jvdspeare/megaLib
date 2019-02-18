1fb2c993431cdf917b39d0c34e7f94c215c5d0c9
jvdspeare <jvdspeare@gmail.com>
2019-02-18 15:06:29 +0000

    Megaport API Python Library

--- examples/logout.py
+++ examples/logout.py
@@ -1,3 +1,13 @@
+# Import megalib
 import megalib
-x = megalib.logout(input('token'), False)
-print(x[0], x[1])
+
+# logout an existing token using the megalib.logout function
+auth = megalib.logout(input('token: '), prod=False)
+
+# Check if logout was successful by observing the HTTP Status Code
+if auth.status_code == 200:
+    print('logout successful')
+
+# Advise user if logout failed
+else:
+    print('logout failed')
