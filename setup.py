5d438cd2bc5c71ab9964155db33347fbeff34aad
jvdspeare <jvdspeare@gmail.com>
2019-02-19 15:37:10 +0000

    Megaport API Python Library

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='megaLib',
+    version='0.9',
+    packages=[''],
+    url='https://github.com/jvdspeare/megaLib',
+    license='MIT',
+    author='james.speare',
+    author_email='jspeare95@gmail.com',
+    description='https://github.com/jvdspeare/megaLib'
+)
