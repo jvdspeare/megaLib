bd8d7251352fb628469d40ac6a4a6bfd490bd12b
jvdspeare <jvdspeare@gmail.com>
2019-02-19 11:42:06 +0000

    Megaport API Python Library

--- examples/service_key.py
+++ examples/service_key.py
@@ -1,8 +1,26 @@
+# Import megalib
 import megalib
-x = megalib.login(input('username'), input('password'))
 
-if x[0] == 200:
-    y = megalib.service_key(x[2], 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 'test service key')
-    print(y[1].json())
+# Authenticate user credentials using the megalib.login function
+auth = megalib.login(input('username: '), input('password: '), input('tfa (leave black if not enabled): '), prod=True)
+
+# Check if logging was successful by observing the HTTP Status Code
+if auth.status_code == 200:
+    print('login successful')
+
+    # Create a service keys for a particular port using the megalib.service_key
+    key = megalib.service_key(auth.header, input('port uid: '), input('description: '), input('vlan: '),
+                              input('single use: '), input('max speed: '), input('pre-approved: '), input('active: '),
+                              input('start time: '), input('end time: '), prod=True)
+
+    # Advise user if call successful
+    if key.status_code == 200:
+        print('key created successfully')
+
+    # Advise user if call failed
+    else:
+        print('failed to create service key')
+
+# Advise user if login failed
 else:
-    print('failed login')
+    print('login failed')
--- examples/service_key_lookup.py
+++ examples/service_key_lookup.py
@@ -1,8 +1,24 @@
+# Import megalib
 import megalib
-x = megalib.login(input('username'), input('password'))
 
-if x[0] == 200:
-    y = megalib.service_key_lookup(x[2], 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx')
-    print(y[1].json())
+# Authenticate user credentials using the megalib.login function
+auth = megalib.login(input('username: '), input('password: '), input('tfa (leave black if not enabled): '), prod=True)
+
+# Check if logging was successful by observing the HTTP Status Code
+if auth.status_code == 200:
+    print('login successful')
+
+    # lookup service keys for a particular port using the megalib.service_key_function
+    key = megalib.service_key_lookup(auth.header, input('port uid: '), prod=True)
+
+    # Print response if call successful
+    if key.status_code == 200:
+        print(key.json)
+
+    # Advise user if lookup failed
+    else:
+        print('service key lookup failed')
+
+# Advise user if login failed
 else:
-    print('failed login')
+    print('login failed')
