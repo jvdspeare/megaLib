e4f3100a17debe402b1f4ad601dd48a827a6d2e8
jvdspeare <jvdspeare@gmail.com>
2019-03-21 10:40:07 +0000

    mcr_to_cloud_simple_demo added

---
+++ examples/demos/mcr_to_cloud_simple_demo.py
@@ -0,0 +1,15 @@
+# Import megalib
+import megalib
+
+# Authenticate user credentials using the megalib.login function
+auth = megalib.login(input('Username: '), input('Password: '), input('TFA (Leave black if not enabled): '), prod=False)
+
+# Order MCR
+mcr = megalib.mcr(auth.header, loc_id, 'megaLib MCR', 100, 'AU')
+
+# Order AWS VXC
+vxc = megalib.aws(auth.header, mcr.uid, b_uid, 'megaLib VXC', 100, 7224, account_num)
+
+# Check for 'Great Success'
+if mcr.status_code and vxc.status_code == 200:
+    print('Great Success!')
