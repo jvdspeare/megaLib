21732729054ecbad5f1eb1fe45d071b6bf897b92
jvdspeare <jvdspeare@gmail.com>
2019-03-27 10:24:34 +0000

    demo things

--- examples/demos/aws_available_demo.py
+++ examples/demos/aws_available_demo.py
@@ -4,16 +4,21 @@ import megalib
 # Authenticate user credentials using the megalib.login function
 auth = megalib.login(input('username: '), input('password: '), input('tfa (leave black if not enabled): '), prod=True)
 
-# Retrieve partner ports using the megalib.partner function
-partner_loc = megalib.partner(auth.header, prod=True)
+# Get locations and partner locations
+loc = megalib.locations(auth.header)
+partner_loc = megalib.partner(auth.header)
 
-# Check if the partner ports call was successful by observing the HTTP Status Code
-if partner_loc.status_code == 200:
+# Check if locations where retrieved successful
+if loc.status_code and partner_loc.status_code == 200:
+    print('Locations retrieved successfully')
+    loc_data = loc.json['data']
     partner_loc_data = partner_loc.json['data']
 
-    for L in partner_loc_data:
-        if L['connectType'] == 'AWS' and L['vxcPermitted'] is True:
-            print(L['productUid'] + ' ' + str(L['locationId']))
+    for P in loc_data:
+        for L in partner_loc_data:
+            if L['connectType'] == 'AWS' and L['vxcPermitted'] is True:
+                if L['locationId'] == P['id']:
+                    print(P['name'] + ' ' + L['title'] + ' ' + L['productUid'] + ' ' + str(L['locationId']))
 
 # Advise user if the partner ports call failed
 else:
