34baccb2d4b90108de3d62dba114b78c6f34ba7f
jvdspeare <jvdspeare@gmail.com>
2019-04-03 18:49:17 +0000

    readme & setup.py update

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='megaLib',
-    version='0.9.3.1',
+    version='0.9.3.2',
     py_modules=['megalib'],
     install_requires=['requests'],
     url='https://github.com/jvdspeare/megaLib',
     license='GPLv3',
     author='james.speare',
     author_email='info.megalib@gmail.com',
-    description='megaLib is a Python wrapper for the Megaport RESTful API'
+    description='megaLib is a Python wrapper for the Megaport RESTful API',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
