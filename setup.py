d3fbcd2e562a310dd25953ab5796168d1ab1b99a
jvdspeare <jvdspeare@gmail.com>
2019-04-03 19:15:01 +0000

    setup.py update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
-from codecs import open
-
-with open('README.md', 'r', 'utf-8') as f:
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
