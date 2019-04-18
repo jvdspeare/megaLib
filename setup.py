dcee449a5b6c1bbf83cbc6768dff136ca97c9cee
jvdspeare <jvdspeare@gmail.com>
2019-04-18 12:50:28 +0000

    setup.py version update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='megaLib',
-    version='0.9.4.1',
+    version='0.9.4.2',
     py_modules=['megalib'],
     install_requires=['requests'],
     url='https://github.com/jvdspeare/megaLib',
