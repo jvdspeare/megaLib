3ee8cbca71f143665b62172d08cb3bf1d3d3ed93
kinshasha <github@knm.com.au>
2020-02-21 08:48:08 +1100

    Update cx types, convert to CSV output
    Include SFDC, ALIBABA and AWSHC cx types
    Change output to CSV for easier import and manipulation of data

---
+++ demos/available_endpoints_demo_csv.py
@@ -0,0 +1,54 @@
+# Import megalib & getpass
+import megalib
+import getpass
+
+# Authenticate user credentials using the megalib.login function
+auth = megalib.login(input('Username: '), getpass.getpass(), input('TFA (Optional): '))
+
+# Observe the HTTP Status Code and advise user if login was successful
+if auth.status_code == 200:
+    print('Login Successful')
+
+    # Get locations and partner locations
+    loc = megalib.locations(auth.header)
+    partner_loc = megalib.partner(auth.header)
+
+    # Check if locations where retrieved successful
+    if loc.status_code and partner_loc.status_code == 200:
+        print('Locations retrieved successfully')
+
+        # Define lists and loop
+        providers = ['AWS', 'AZURE', 'GOOGLE', 'ORACLE', 'AWSHC', 'SFDC', 'ALIBABA', 'Show', 'Show & Quit']
+        connect_type = list()
+        loop = True
+
+        while loop is True:
+            select = True
+
+            # Menu
+            while select is True:
+                for idx, provider in enumerate(providers):
+                    print(idx, provider)
+
+                x = int(input('Select the provider: '))
+                if x == 8:
+                    select = False
+                elif x == 9:
+                    select = False
+                    loop = False
+                else:
+                    connect_type.append(providers[x])
+
+            # Find results, format and print results
+            for N in connect_type:
+                for P in loc.json['data']:
+                    for L in partner_loc.json['data']:
+                        if L['connectType'] == N:
+                            if L['vxcPermitted'] is True:
+                                if L['locationId'] == P['id']:
+                                    print(L['connectType'] + ',' + P['name'] + ',' + L['title'] + ',' +
+                                          L['productUid'] + ',' + str(L['locationId']))
+
+# Advise user if login failed, print the status code & JSON
+else:
+    print('Login Failed, Status Code: ' + str(auth.status_code) + ', JSON: ' + str(auth.json))
